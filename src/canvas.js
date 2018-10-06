f50ac408865ceafee9f00384417a0e7bc238eb18
serverhiccups <hiccup@hiccup01.com>
2018-10-06 20:58:56 +1300

    Change Canvas size
    This is so that 3 divides easily into it

--- src/canvas.js
+++ src/canvas.js
@@ -1,11 +1,11 @@
 class Canvas {
 	constructor() {
 		let newCanvas = [];
-		for(let i = 0; i < 640; i++) {
+		for(let i = 0; i < 642; i++) {
 			newCanvas.push([]);
 		}
 		newCanvas.forEach((e) => {
-			for(let j = 0; j < 640; j++) {
+			for(let j = 0; j < 642; j++) {
 				e.push(0xFFFFFF);
 			}
 		});

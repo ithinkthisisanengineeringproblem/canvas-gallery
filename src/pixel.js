e43c06b7be9c15691656126a4448b1c8b2b35fa0
serverhiccups <hiccup@hiccup01.com>
2018-10-07 09:22:13 +1300

    Add library to make dealing with pixel messages easier

---
+++ src/pixel.js
@@ -0,0 +1,12 @@
+let Pixel = {
+	serialise: function(x, y, colour) {
+		return `${x};${y};${colour.toString(16)}`
+	},
+	deserialise: function(input) {
+		let elements = input.split(';');
+		elements[0] = parseInt(elements[0], 10);
+		elements[1] = parseInt(elements[1], 10);
+		elements[2] = parseInt(elements[2], 16);
+		return elements
+	}
+}

ed268e15d8fc6b7b687f6aa7add3d4ed39f260ea
serverhiccups <hiccup@hiccup01.com>
2018-10-07 11:29:19 +1300

    Update Server to match the new standard

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 let path = require("path");
+let Pixel = require("./src/pixel.js");
 let Canvas = require('./src/canvas.js');
 let canvas = new Canvas();
 
@@ -11,7 +12,10 @@ server.listen(80);
 app.use("/", express.static(path.join(__dirname, "public")));
 
 io.on('connection', (client) => {
-	client.on('update', (data) => {
-		client.emit('echo', data);
+	client.emit("clear", canvas.deserialiseCanvas());
+	client.on('request', (data) => {
+		let properties = Pixel.deserialise(data);
+		canvas.setPixel(properties[0], properties[1], properties[2]);
+		io.emit("update", data);
 	})
 })

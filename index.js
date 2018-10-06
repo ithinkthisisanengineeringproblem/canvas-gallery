70211e98580b04094a1b75307886fc85e781802e
serverhiccups <hiccup@hiccup01.com>
2018-10-06 20:52:55 +1300

    Make sure it's possible to import the Canvas object

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 let path = require("path");
+let Canvas = require('./src/canvas.js');
+let canvas = new Canvas();
 
 server.listen(80);
 

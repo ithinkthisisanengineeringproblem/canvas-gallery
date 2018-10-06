70f4438013bff8daafba7529d252001451337e05
serverhiccups <hiccup@hiccup01.com>
2018-10-06 13:07:34 +1300

    Make the server serve files in a sane way

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-let app = require('express')();
+let express = require('express');
+let app = express(); 
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
+let path = require("path");
 
 server.listen(80);
 
-app.get('/', (req, res) => {
-	res.sendfile(__dirname + '/frontend/index.html');
-});
-
-app.get('/index.js', (req, res) => {
-	res.sendfile(__dirname + '/frontend/index.js');
-});
+app.use("/", express.static(path.join(__dirname, "public")));
 
 io.on('connection', (client) => {
 	client.on('update', (data) => {

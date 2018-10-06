609a89ba05f74ac0c388b4d1e2b03913dc3373fc
serverhiccups <hiccup@hiccup01.com>
2018-10-06 15:36:30 +1300

    Acceptable Frontend
    Adds the required JS for *very* basic drawing

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,14 @@
-"use strict";
-
 //initialize websocket with server
 const socket = io("http://localhost:80");
 socket.on('echo', (data) => {
 console.log(data);
 });
 socket.emit('update', 'hello');
+let canvas = document.querySelector("#content").childNodes[1];
+let ctx = canvas.getContext('2d');
+canvas.onmousedown = (e) => {
+	let x = e.pageX - e.currentTarget.offsetLeft;
+	let y = e.pageY - e.currentTarget.offsetTop;
+	//alert(`mouseX: ${x}, mouseY: ${y}`);
+	e.currentTarget.getContext('2d').fillRect(x, y, 2, 2);
+};

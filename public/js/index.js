9d15f71695f04f4e1a87b6960bd56a4d702316e4
serverhiccups <hiccup@hiccup01.com>
2018-10-20 16:22:31 +1300

    Make the website up to standard
    Also adds a colour picker

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,8 @@ let canvas = new Canvas();
 //attach canvas to DOM canvas
 canvas.attachToDOMCanvas(document.getElementById('mainCanvas'));
 
+let paintColour = 0x000000;
+
 //on update from websocket server
 socket.on('update', (data) => { 
 	//deserialise data from server
@@ -29,5 +31,21 @@ document.getElementById('mainCanvas').onmousedown = (e) => {
 	let x = Math.floor((e.pageX - e.currentTarget.offsetLeft)/4);
 	let y = Math.floor((e.pageY - e.currentTarget.offsetTop)/4);
 	//send pixel-change data to websocket server 
-	socket.emit('request', Pixel.serialise(y, x, 0xFF00FF));
+	socket.emit('request', Pixel.serialise(y, x, paintColour));
 }
+
+let colourList = [0x000000, 0xFFFFFF, 0x00FF00, 0x0000FF, 0xFF0000, 0xFFFF00, 0xFF00FF, 0x00FFFF];
+
+for (let colour of colourList) {
+	let button = document.createElement("button");
+	button.className = "pickerButton";
+	button.style.backgroundColor = "#" + colour.toString(16).padStart(6, "0");
+	button.style.border = "none";
+	button.addEventListener("mousedown", (e) => {
+		paintColour = colour;
+	})
+	document.getElementById("controls").appendChild(button);
+}
+
+document.getElementById("controls").style["max-width"] = colourList.length * 25 + "px";
+document.getElementById("controls").style["min-width"] = colourList.length * 25 + "px";
